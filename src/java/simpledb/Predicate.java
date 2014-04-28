01a88d10b110eecc2cfd699e5b869b5b0bc342d9
Andy Yoon <ayoon212@gmail.com>
2014-04-27 20:47:55 -0700

    Predicate and Filter done

--- src/java/simpledb/Predicate.java
+++ src/java/simpledb/Predicate.java
@@ -9,6 +9,10 @@ public class Predicate implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    private int m_field;
+    private Op m_op;
+    private Field m_operand;
+
     /** Constants used for return codes in Field.compare */
     public enum Op implements Serializable {
         EQUALS, GREATER_THAN, LESS_THAN, LESS_THAN_OR_EQ, GREATER_THAN_OR_EQ, LIKE, NOT_EQUALS;
@@ -55,7 +59,9 @@ public class Predicate implements Serializable {
      *            field value to compare passed in tuples to
      */
     public Predicate(int field, Op op, Field operand) {
-        // some code goes here
+        m_field = field;
+        m_op = op;
+        m_operand = operand;
     }
 
     /**
@@ -63,8 +69,7 @@ public class Predicate implements Serializable {
      */
     public int getField()
     {
-        // some code goes here
-        return -1;
+        return m_field;
     }
 
     /**
@@ -72,8 +77,7 @@ public class Predicate implements Serializable {
      */
     public Op getOp()
     {
-        // some code goes here
-        return null;
+        return m_op;
     }
     
     /**
@@ -81,8 +85,7 @@ public class Predicate implements Serializable {
      */
     public Field getOperand()
     {
-        // some code goes here
-        return null;
+        return m_operand;
     }
     
     /**
@@ -96,8 +99,7 @@ public class Predicate implements Serializable {
      * @return true if the comparison is true, false otherwise.
      */
     public boolean filter(Tuple t) {
-        // some code goes here
-        return false;
+        return t.getField(m_field).compare(m_op, m_operand);
     }
 
     /**
@@ -105,7 +107,6 @@ public class Predicate implements Serializable {
      * operand_string
      */
     public String toString() {
-        // some code goes here
-        return "";
+        return "f = " + m_field + " op = " + m_op.toString() + " operand = " + m_operand.toString() + "\n";
     }
 }
