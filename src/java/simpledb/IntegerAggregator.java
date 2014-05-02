d243ee511362c9d1e62e25382d4c90e860e830a9
Andy Yoon <ayoon212@gmail.com>
2014-05-01 20:41:51 -0700

    started

--- src/java/simpledb/IntegerAggregator.java
+++ src/java/simpledb/IntegerAggregator.java
@@ -1,11 +1,20 @@
 package simpledb;
 
+import java.util.*;
+
 /**
  * Knows how to compute some aggregate over a set of IntFields.
  */
 public class IntegerAggregator implements Aggregator {
 
     private static final long serialVersionUID = 1L;
+
+    private int m_gbfield;
+    private Type m_gbfieldtype;
+    private int m_afield;
+    private Op m_what;
+
+    private HashMap<Object, Integer> m_grouping; // Maps the group-by field to its aggregate value
 
     /**
      * Aggregate constructor
@@ -23,7 +32,19 @@ public class IntegerAggregator implements Aggregator {
      */
 
     public IntegerAggregator(int gbfield, Type gbfieldtype, int afield, Op what) {
-        // some code goes here
+        m_gbfield = gbfield;
+        m_gbfieldtype = gbfieldtype;
+        m_afield = afield;
+        m_what = what;
+        m_grouping = new HashMap<Object, Integer>();
+    }
+
+    private int initial_value() {
+        switch(m_what) {
+            case MIN: return Integer.MAX_VALUE;
+            case MAX: return Integer.MIN_VALUE;
+            default: return 0; // SUM, AVG, COUNT
+        }
     }
 
     /**
@@ -34,7 +55,18 @@ public class IntegerAggregator implements Aggregator {
      *            the Tuple containing an aggregate field and a group-by field
      */
     public void mergeTupleIntoGroup(Tuple tup) {
-        // some code goes here
+        Object key;
+        if (m_gbfield == NO_GROUPING || m_gbfieldtype == null) { key = null; }
+        else { Object key = tup.getField(m_gbfield); }
+
+        if (!m_grouping.containsKey(key)) {
+            // Haven't encountered this group value yet, create new grouping
+            m_grouping.put(key, initial_value());
+        }
+        int val = m_grouping.get(key);
+        int field_val = key.getValue();
+
+
     }
 
     /**
